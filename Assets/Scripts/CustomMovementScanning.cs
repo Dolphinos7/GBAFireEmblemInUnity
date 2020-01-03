5ecc530f58562b396438a6a59c9f3bc52d72d726
Dolphinos <33300007+Dolphinos7@users.noreply.github.com>
2020-01-03 02:32:22 -0500

    TryingToDeleteStuff

--- Assets/Scripts/CustomMovementScanning.cs
+++ Assets/Scripts/CustomMovementScanning.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//TestingComment
 public class CustomMovementScanning : MonoBehaviour
 {
     List<Tile> movableTiles = new List<Tile>();
